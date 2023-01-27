8a48c52111fbcd6944f9368f4a9526d8f2cb0361
jyshgupta <jyshgupta@gmail.com>
2023-01-27 13:55:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Diner')
+Omega 3 & Blueberry Oatmeal
+Kale, Spinach & Rocket Smoothie
+Hard-Boiled Free-Range Eggs
